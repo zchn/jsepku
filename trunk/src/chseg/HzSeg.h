d3bf400b7d4b5704b1b91585ef9806cdd82910c8
joyanlj <joyanlj@702d1322-cd41-0410-9eed-0fefee27d168>
2007-12-21 15:23:49 +0000

    HeiHei! A tiny se called Jse

---
+++ trunk/src/chseg/HzSeg.h
@@ -0,0 +1,25 @@
+#ifndef _HZSEG_H_040415_
+#define _HZSEG_H_040415_
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Dict.h"
+
+using namespace std;
+
+class CHzSeg
+{
+public:
+	CHzSeg();
+	~CHzSeg();
+
+	string SegmentSentenceMM (CDict&, string) const;
+	string SegmentHzStrMM (CDict&, string) const;
+	string SegmentURL(CDict&, string) const;
+
+	// process a sentence before segmentation
+	void Translate(char* SourceStr) const;
+};
+	
+#endif /* _HZSEG_H_040415_ */
