08394c28026f40faa83ea37130723bfa4f4b4fb5
joyanlj <joyanlj@702d1322-cd41-0410-9eed-0fefee27d168>
2007-12-22 06:57:49 +0000

    Wrote the subroutine rawseg. Change the p3_plan.(put the rawseg after docoff, because it needs the file xxx.didx.

---
+++ trunk/doc/auto/p3_plan.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook "p3_plan"
+ (lambda ()
+    (TeX-run-style-hooks
+     "xcolor"
+     "listings"
+     "zhfontcfg"
+     "latex2e"
+     "art10"
+     "article"
+     "14pt"
+     "a4paper")))
+
