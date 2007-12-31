bb4d78f3b3f68d66c76dc55ad7e9e31150257737
joyanlj <joyanlj@702d1322-cd41-0410-9eed-0fefee27d168>
2007-12-31 15:16:16 +0000

    Insert comments

--- trunk/src/invert.cpp
+++ trunk/src/invert.cpp
@@ -1,3 +1,22 @@
+// 我真诚地保证：
+    
+// 我自己独立地完成了整个程序从分析、设计到编码的所有工作。
+// 如果在上述过程中，我遇到了什么困难而求教于人，那么，我将在程序实习报告中
+// 详细地列举我所遇到的问题，以及别人给我的提示。
+
+// 在此，我感谢 XXX, …, XXX对我的启发和帮助。下面的报告中，我还会具体地提到
+// 他们在各个方法对我的帮助。
+ 
+// 我的程序里中凡是引用到其他程序或文档之处，
+// 例如教材、课堂笔记、网上的源代码以及其他参考书上的代码段,
+// 我都已经在程序的注释里很清楚地注明了引用的出处。
+
+// 我从未没抄袭过别人的程序，也没有盗用别人的程序，
+// 不管是修改式的抄袭还是原封不动的抄袭。
+
+// 我编写这个程序，从来没有想过要去破坏或妨碍其他计算机系统的正常运转。
+    
+// 陈志杰 揭忠
 #include<iostream>
 
 #include<fstream>
@@ -10,6 +29,12 @@ struct word_info
 	string words;
 	string id;
 };
+/**********************************************************************
+函数名：invert
+功能：将.piidx.sort类型的文件转化为.iidx类型的文件，有关文件类型的说明请参考解题报告。
+输入参数：一个string类型的文件路径。
+返回值：如果成功则返回0，否则返回-1。
+***************************************************************************/
 int invert(string path)
 {
 	ifstream in_piidx_sort(path.c_str());
@@ -18,6 +43,7 @@ int invert(string path)
 		cerr<<"[ERROR]:in-file can't be opened"<<endl;
 		return -1;
 	}
+	/*将文件名中的.piidx.sort转化为.iidx*/
 	word_info *words_info;
 	words_info=new word_info[2];
 	string out_name=path;
@@ -30,6 +56,7 @@ int invert(string path)
 	in_piidx_sort>>words_info[0].id;
 	out_iidx<<setw(TERM_LEN)<<left<<words_info[0].words;
 	out_iidx<<setw(TERM_LEN)<<left<<words_info[0].id;
+	/*按照.iidx的要求从左开始输出*/
 	while(!in_piidx_sort.eof())
 	{
 		in_piidx_sort>>words_info[1].words;
@@ -50,6 +77,7 @@ int invert(string path)
 	}
 	return 0;
 }
+
 int main(int argc,char *argv[])
 {
 	string file_name=argv[1];
