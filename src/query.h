ae227781e94d633499966cad3c18731cd670675d
joyanlj <joyanlj@702d1322-cd41-0410-9eed-0fefee27d168>
2007-12-31 07:06:03 +0000

    UnitTest for serv and query

--- src/query.h
+++ src/query.h
@@ -38,7 +38,7 @@ public:
   c_query(const string path)
   {
     iidx_file.open(path.c_str());
-    if(!iidx_file.is_open()){
+    if(!iidx_file){
       cerr<<"[ERROR]:file "<<path<<" can't be opened"<<endl;
     }
     creat_map();
@@ -48,10 +48,16 @@ public:
     iidx_file.close();
   }
 
+  void out_debug(string out){
+    cerr<<"[DEBUG]:"<<out<<endl;
+  }
+  
   void query(const string words,vector<struct s_result> &res)
   {
+    res.clear();
     string m_words;
     m_words=cut_words(words);
+    out_debug("words cuted :"+cuted);
     string keywords;
     unsigned int i=0;
     while(i<m_words.length()){
@@ -62,12 +68,12 @@ public:
       }
       string record=get_term(keywords);
       if(record == ""){
-        res.clear();
         return;
       }
       vector<s_result> temp_vector;/*this temp is what i will give to your function*/
       process_term(record,temp_vector);
       res_merge(res,temp_vector);/*this maybe wrong*/
+      while(i < m_words.length() && m_words[i] == ' ') i++;
     }
   }
  
@@ -85,12 +91,20 @@ public:
   {
     string word;
     int site;
+    out_debug("begin create_map");
     while(!iidx_file.eof()){
       site=iidx_file.tellg();
       iidx_file>>word;
       map_word.insert(pair<string,int>(word,site));
       getline(iidx_file,word);
-    }     
+    }
+    iidx_file.clear();
+    out_debug("map created!");
+    // map<string,int>::iterator it;
+    // for(it = map_word.begin(); it != map_word.end(); it++){
+    //   cerr<<"[DEBUG]:map has "<<it->first<<' '<<it->second<<endl;
+    // }
+
   }
 
   string get_term(const string word)
@@ -101,36 +115,46 @@ public:
     if(item == map_word.end())
       return string("");
     int site=item->second;
-    iidx_file.seekg(site);
+    iidx_file.seekg(site,ios_base::beg);
+    if(!iidx_file){
+      cerr<<"[ERROR]:iidx file broken when seeking word term!"<<endl;
+    }
     getline(iidx_file,words);
-    cerr<<"[DEBUG]:find word "<<words<< " !"<<endl;
+    cerr<<"[DEBUG]:find word "<<words<<" for "<<word<<" at "<<site<< " !"<<endl;
     return words;
   }
 
   void process_term(const string record,
                     vector<struct s_result> &sub_result)
   {
+    cerr<<"[DEBUG]:Extracting docids from "<<record<<endl;
     istrstream rec(record.c_str());
     s_result tmp_res;
+    string head;
     int tmp;
     sub_result.clear();
     if(rec){
-      rec>>tmp;
+      rec>>head;
+      cout<<"[DEBUG]:head has "<<head<<endl;
     }else{
       cerr<<"[ERROR]:record "<<record<<" invalid"<<endl;
       return;
     }
     rec>>hex>>tmp_res.docid;
     tmp_res.weight = 1;
-    while(rec>>hex>>tmp){
+    while(rec){
+      rec>>hex>>tmp;
       if(tmp_res.docid == tmp){
         tmp_res.weight++;
       }else{
+        cerr<<"[DEBUG]:Get a doc "<<tmp_res.docid<<'-'<<tmp_res.weight<<endl;
         sub_result.push_back(tmp_res);
         tmp_res.docid = tmp;
         tmp_res.weight = 1;
       }
     }
+    cerr<<"[DEBUG]:Get a doc "<<tmp_res.docid<<'-'<<tmp_res.weight<<endl;
+    sub_result.push_back(tmp_res);
   }    
     // int len;
     // len=record.length();
@@ -176,9 +200,11 @@ public:
   void res_merge(vector<struct s_result> &final,
                  vector<struct s_result> &sub_result)
   {
+    out_debug("Now I'm in res_merge()");
     sort(sub_result.begin(),sub_result.end(),comp_docid);
     if(final.size() == 0){
       final = sub_result;
+      out_debug("Now I'm leaving res_merge()");
       return;
     }else{
       vector<struct s_result>::iterator it_final,it_sub;
@@ -194,7 +220,8 @@ public:
         }
       }
     }
-  }    
+    out_debug("Now I'm leaving res_merge()");
+  }
 };
 
 #endif
--- src/serv.cpp
+++ src/serv.cpp
@@ -29,31 +29,42 @@ int docid2offset(int id)
   string tmp;
   getline(didx_file,tmp);
   int t,ret;
-  sscanf(tmp.c_str(),"%x %x",&t,&ret);
+  if(sscanf(tmp.c_str(),"%x %x",&t,&ret)!= 2){
+    cerr<<"[ERROR]:docid to offset failed! "<<id<<endl;
+  }
   return ret;
 }
 
 void get_content(int offset,string &content){
+  cerr<<"[DEBUG]:Now I'm in get_content("<<offset<<",content)"<<endl;
   content.clear();
+  rawfile.clear();
   rawfile.seekg(offset,ios_base::beg);
   string tmp;
+  if(!rawfile){
+    cerr<<"[ERROR]:RAW file broken!"<<endl;
+  }
   getline(rawfile,tmp);
+  cerr<<"[DEBUG]:read firstline from rawfile offset "<<tmp<<endl;
   if(tmp != "version:1.0"){
     cerr<<"[ERROR]:I read "<<tmp
         <<" first while expecting verson when getting the content acorrding to offset!"<<endl;
   }
-  content += tmp;
+  content += tmp+'\n';
   getline(rawfile,tmp);
-  while(tmp != "verson:1.0"){
-    content += tmp;
+  while(rawfile && tmp != "verson:1.0"){
+    content += tmp + '\n';
+    //cerr<<"[DEBUG]:read from rawfile offset "<<tmp<<endl;
     getline(rawfile,tmp);
   }
+  cerr<<"[DEBUG]:Now I'm leaving get_content"<<endl;
 }
 
 void get_title_url(istrstream &cont,c_raw_record &rec){
   string tmp;
-  
+  cerr<<"[DEBUG]:Now I'm in get_title_url()"<<endl;
   getline(cont,tmp);
+  cerr<<"[DEBUG]:first str: "<<tmp<<endl;
   while(tmp.substr(0,4) != "url:")
     getline(cont,tmp);
 
@@ -63,32 +74,37 @@ void get_title_url(istrstream &cont,c_raw_record &rec){
     pos = tmp.rfind(':');
   }
   rec.title = tmp.substr(pos+1);
+  cerr<<"[DEBUG]:Now I'm leaving get_title_url(). Got "<<rec.title<<'-'<<rec.url<<endl;
 }
 
 void get_digest(const string cuted,const string &content,istrstream &cont,c_raw_record &rec){
+  cerr<<"[DEBUG]:Now I'm in get_digest()."<<endl;
   int *beg = new int[content.size()];
   int *end = new int[content.size()];
   memset(beg,0,sizeof(int)*content.size());
   memset(end,0,sizeof(int)*content.size());
   rec.digest = "";
+  istrstream cuted_s(cuted.c_str());
   string tmp;
-  while(cont){
-    cont>>tmp;
-    if(cuted.find(tmp) != string::npos){
-      unsigned int i,pos = cont.tellg();
-      for(i = pos-1; pos-i < 15; i--){
+  while(cuted_s){
+    cuted_s>>tmp;
+    unsigned int pos = 0;
+    while((pos = content.find(tmp,pos)) != string::npos){
+      unsigned int i;
+      for(i = pos-1; pos-i < 10; i--){
         if((unsigned char)content[i] >= 128)
           i--;
       }
       i++;
       beg[i]++;
 
-      for(i = pos; i-pos < 10 && i < content.size(); i++){
+      for(i = pos; i-pos < 15 && i < content.size(); i++){
         if((unsigned char)content[i] >= 128)
           i++;
       }
       i--;
       end[i]++;
+      pos++;
     }
   }
   unsigned int j;
@@ -97,26 +113,36 @@ void get_digest(const string cuted,const string &content,istrstream &cont,c_raw_
     print += beg[j];
     print -= end[j];
     if(print){
-      rec.digest += content[j];
+      if(content[j] < ' ')
+        rec.digest += ' ';
+      else
+        rec.digest += content[j];
     }
   }
+  cerr<<"[DEBUG]:Now I'm leaving get_digest()."<<endl;
 }
   
 
 
 void make_rec(string cuted, s_result res,c_raw_record &rec)
 {
+  cerr<<"[DEBUG]:Now I'm in make_rec("<<cuted<<")"<<endl;
   int offset = docid2offset(res.docid);
   string content;
-  istrstream cont(content.c_str());
   get_content(offset,content);
+  istrstream cont(content.c_str());
+  if(!cont){
+    cerr<<"[ERROR]:Cannot init istrstream~"<<endl;
+  }
   get_title_url(cont,rec);
   get_digest(cuted,content,cont,rec);
+  cerr<<"[DEBUG]:Now I'm leaving make_rec()"<<endl;
 }
 
 
 void get_telnet_result(string words)
 {
+  cerr<<"[DEBUG]Now I'm in get_telnet_result"<<endl;
   vector<s_result> result;
   query->query(words,result);
   if(result.size() == 0){
@@ -135,7 +161,10 @@ void get_telnet_result(string words)
 }
 
 int main(int argc,char **argv){
-  
+  if(argc != 4){
+    cout<<argv[0]<<" raw iidx didx"<<endl;
+    return 1;
+  }
   string rawfile_name(argv[1]);
   string iidx_file_name(argv[2]);
   string didx_file_name(argv[3]);
@@ -152,9 +181,11 @@ int main(int argc,char **argv){
   query = new c_query(iidx_file_name);
 
   string tmp;
-  cin>>tmp;
+  getline(cin,tmp);
   while(tmp != "@!"){
-    get_telnet_result(tmp);
+    if(tmp != "")
+      get_telnet_result(tmp);
+    getline(cin,tmp);
   }
   return 0;
 }
